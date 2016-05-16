459a89047f1ebfef295b1a24f3577ae4a8814057
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-16 19:20:21 +0300

    Removed unused imports

--- ex1/src/ConsoleReader.java
+++ ex1/src/ConsoleReader.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /**
  * Created by clouway on 16.05.16.
