d2091664a8be67235ce999949dc3466e975b75ee
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-18 12:09:41 +0300

    Formated code

--- ex2/src/CreatedFIle.java
+++ ex2/src/CreatedFIle.java
@@ -15,10 +15,13 @@ public class CreatedFIle {
 
         this.nameOfFile = nameOfFile;
         this.endFileSymbol = endFileSymbol;
-        }
+    }
 
-
+    /**
+     * Creating file and calling write() to write given content.
+     */
     public void createFile() {
+
         if (file == null) {
             File directory = new File("/src");
             file = new File(directory, nameOfFile);
@@ -26,12 +29,20 @@ public class CreatedFIle {
         write();
     }
 
+    /**
+     * Reads content from console.
+     *
+     * @param endFileSymbol symbol used by user to stop read from console.
+     * @return content from console.
+     */
     private String read(String endFileSymbol) {
         Scanner scanner = new Scanner(System.in);
         String content = "";
         boolean check = true;
+
         while (check) {
             String buffer = scanner.nextLine();
+
             if (buffer.equals(endFileSymbol)) {
                 check = false;
             } else {
@@ -41,7 +52,11 @@ public class CreatedFIle {
         return content;
     }
 
+    /**
+     * Write content form read() to file.
+     */
     private void write() {
+
         try (FileWriter writer = new FileWriter(nameOfFile)) {
             writer.write(read(endFileSymbol));
         } catch (IOException e) {
