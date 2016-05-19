f925a7a5e4ba58806d0983c039020c1e95d60c81
Alexander Vladimirov <alexandervladimirov1902@gmail.com>
2016-05-19 16:24:55 +0300

    Dumb Idea.

--- ex3/src/Demo.java
+++ ex3/src/Demo.java
@@ -5,11 +5,17 @@ import java.io.*;
  */
 public class Demo {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+            TextFile file = new TextFile();
 
-
+        try {
+           file.reverse("file");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
+
 }
 
 
